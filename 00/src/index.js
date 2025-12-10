756d5bb65a0c6c236fe133b6385362c0b1bec9e7
GersonESS <gebhsantos@gmail.com>
2025-12-10 11:02:12 -0300

    a

--- 00/src/index.js
+++ 00/src/index.js
@@ -1,28 +1,9 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+const app = express()
+const port = 3000
 
-const app = express();
-const port = 3000;
-const prisma = new PrismaClient();
-
-app.use(express.json());
-
-app.get('/', async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
-
-app.post('/usuarios', async (req, res) => {
-// ...existing code...
-  await prisma.user.create({
-    data: {
-      email: req.body.email,
-      name: req.body.name,
-      age: req.body.age,
-    },
-  })
-
-  res.status(201).json(req.body)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
