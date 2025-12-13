fbf7bec8506aadff00800e339bae4a962bee1940
GersonESS <gebhsantos@gmail.com>
2025-12-12 21:53:19 -0300

    a

--- server/01/index.js
+++ server/01/index.js
@@ -3,7 +3,6 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const app = express()
 const port = 3000
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -11,21 +10,6 @@ app.get('/usuarios', async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
 });
-
-
-async function testConnection() {
-  try {
-    await prisma.$connect()
-    console.log('🚀 conectado com sucesso')
-  } catch (error) {
-    console.log('erro na conecxao', error)
-  }
-}
-
-testConnection()
-
-
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
